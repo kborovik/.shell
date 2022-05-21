8095799178359a4ce8cd23511725c21dca211481
Konstantin Borovik <kb@lab5.ca>
2022-05-20 23:37:03 -0400

    add auto-install script (#3)
    * reanme template file
    
    * add readme.md file
    
    * remove extra icons
    
    * add OneHalf Dark theme
    
    * change prompt color to white
    
    * update docs
    
    * add terminal_background color
    
    * add color palette
    
    * replace all colors with pelate
    
    * add error code to second prompt
    
    * add vim files
    
    * add install-shell.sh script
    
    * update installation script

---
+++ install-shell.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+
+set -x
+
+mkdir -p ${HOME}/.local/bin
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O ${HOME}/.local/bin/oh-my-posh
+chmod +x ${HOME}/.local/bin/oh-my-posh
+
+git clone git@github.com:kborovik/.shell.git
+
+ln -b -s ${HOME}/.shell/.digrc ${HOME}/.digrc
+ln -b -s ${HOME}/.shell/.vimrc ${HOME}/.vimrc
+ln -b -s ${HOME}/.shell/.vim ${HOME}/.vim
+ln -b -s ${HOME}/.shell/.bashrc ${HOME}/.bashrc
