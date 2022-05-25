0bfa9de89d46f44f8c4a0543a327be7a267be32c
Konstantin Borovik <kb@lab5.ca>
2022-05-25 09:20:54 -0400

    update installation script

--- install-shell.sh
+++ install-shell.sh
@@ -2,11 +2,14 @@
 
 set -e
 
-mkdir -p ${HOME}/.local/bin
-wget -q https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O ${HOME}/.local/bin/oh-my-posh
-chmod +x ${HOME}/.local/bin/oh-my-posh
+mkdir -p "${HOME}/.local/bin/" "${HOME}/.gnupg/" "${HOME}/.vim"
 
-ln -b -s ${HOME}/.shell/.digrc ${HOME}/.digrc
-ln -b -s ${HOME}/.shell/.vimrc ${HOME}/.vimrc
-ln -b -s ${HOME}/.shell/.vim ${HOME}/.vim
-ln -b -s ${HOME}/.shell/.bashrc ${HOME}/.bashrc
+wget -q "https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64" -O "${HOME}/.local/bin/oh-my-posh" && chmod +x "${HOME}/.local/bin/oh-my-posh"
+
+ln -b -s "${HOME}/.shell/.bashrc" "${HOME}/.bashrc"
+ln -b -s "${HOME}/.shell/.digrc" "${HOME}/.digrc"
+ln -b -s "${HOME}/.shell/.gnupg/gpg-agent.conf" "${HOME}/.gnupg/gpg-agent.conf"
+ln -b -s "${HOME}/.shell/.gnupg/gpg.conf" "${HOME}/.gnupg/gpg.conf"
+ln -b -s "${HOME}/.shell/.gnupg/scdaemon.conf" "${HOME}/.gnupg/scdaemon.conf"
+ln -b -s "${HOME}/.shell/.vim" "${HOME}/.vim"
+ln -b -s "${HOME}/.shell/.vimrc" "${HOME}/.vimrc"
