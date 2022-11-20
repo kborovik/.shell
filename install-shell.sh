be6002d1bbf80036da35654e3a89e6d81093211e
Konstantin Borovik <kb@lab5.ca>
2022-11-19 23:30:12 -0500

    fix install script macOS (#53)

--- install-shell.sh
+++ install-shell.sh
@@ -4,7 +4,7 @@ set -e
 
 mkdir -p "${HOME}/.local/bin/" "${HOME}/.gnupg/" "${HOME}/.vim/colors/"
 
-wget -q "https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64" -O "${HOME}/.local/bin/oh-my-posh" && chmod +x "${HOME}/.local/bin/oh-my-posh"
+wget -q "https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-$(uname -o)-$(uname -m)" -O "${HOME}/.local/bin/oh-my-posh" && chmod +x "${HOME}/.local/bin/oh-my-posh"
 
 ln -b -s "${HOME}/.shell/.bashrc" "${HOME}/.bashrc"
 ln -b -s "${HOME}/.shell/.digrc" "${HOME}/.digrc"
