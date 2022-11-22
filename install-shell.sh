ca8649f5637608327d10f36f7298e776529bf2ed
Konstantin Borovik <kb@lab5.ca>
2022-11-22 17:00:04 -0500

    add cosign verify signatures

--- install-shell.sh
+++ install-shell.sh
@@ -13,14 +13,34 @@ mkdir -p "${dirs[@]}"
 case $(uname) in
 Linux)
   posh_binary="posh-linux-amd64"
+  cosign_binary="cosign-linux-amd64"
   ;;
 Darwin)
   posh_binary="posh-darwin-$(uname -m)"
+  cosign_binary="cosign-darwin-$(uname -m)"
   ;;
 esac
 
-wget -q "https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/$posh_binary" -O "$HOME/.local/bin/oh-my-posh" && chmod +x "$HOME/.local/bin/oh-my-posh"
+echo "==> Download cosign"
+wget -q "https://github.com/sigstore/cosign/releases/latest/download/$cosign_binary.sig" -O "$HOME/.local/bin/$cosign_binary.sig" || exit 1
+wget -q "https://github.com/sigstore/cosign/releases/latest/download/$cosign_binary" -O "$HOME/.local/bin/cosign" && chmod +x "$HOME/.local/bin/cosign" || exit 1
 
+echo "==> Verify cosign signature"
+cosign verify-blob --key "cosign/cosign.pub" --signature "$HOME/.local/bin/$cosign_binary.sig" "$HOME/.local/bin/cosign" && rm "$HOME/.local/bin/$cosign_binary.sig" || exit 1
+
+echo "==> Download oh-my-posh"
+wget -q "https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/$posh_binary.sig" -O "$HOME/.local/bin/$posh_binary.sig" || exit 1
+wget -q "https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/$posh_binary" -O "$HOME/.local/bin/$posh_binary" && chmod +x "$HOME/.local/bin/$posh_binary" || exit 1
+
+echo "==> Verify oh-my-posh signature"
+if cosign verify-blob --key "cosign/oh-my-posh.pub" --signature "$HOME/.local/bin/$posh_binary.sig" "$HOME/.local/bin/$posh_binary"; then
+  mv "$HOME/.local/bin/$posh_binary" "$HOME/.local/bin/oh-my-posh"
+  rm "$HOME/.local/bin/$posh_binary.sig"
+else
+  exit 1
+fi
+
+echo "==> Link config files"
 files=(
   .bashrc
   .config/helix/config.toml
@@ -36,4 +56,3 @@ files=(
 for file in "${files[@]}"; do
   ln -b -s "$HOME/.shell/$file" "$HOME/$file"
 done
-
