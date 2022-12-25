a136ccb91bcaf8cc7e7b38bfa23a850e326afd2b
Konstantin Borovik <kb@lab5.ca>
2022-12-25 12:08:22 -0500

    add explisit path cosign

--- install-shell.sh
+++ install-shell.sh
@@ -26,14 +26,14 @@ wget -q "https://github.com/sigstore/cosign/releases/latest/download/$cosign_bin
 wget -q "https://github.com/sigstore/cosign/releases/latest/download/$cosign_binary" -O "$HOME/bin/cosign" && chmod +x "$HOME/bin/cosign" || exit 1
 
 echo "==> Verify cosign signature"
-cosign verify-blob --key "$HOME/.shell/cosign/cosign.pub" --signature "$HOME/bin/$cosign_binary.sig" "$HOME/bin/cosign" && rm "$HOME/bin/$cosign_binary.sig" || exit 1
+~/bin/cosign verify-blob --key "$HOME/.shell/cosign/cosign.pub" --signature "$HOME/bin/$cosign_binary.sig" "$HOME/bin/cosign" && rm "$HOME/bin/$cosign_binary.sig" || exit 1
 
 echo "==> Download oh-my-posh"
 wget -q "https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/$posh_binary.sig" -O "$HOME/bin/$posh_binary.sig" || exit 1
 wget -q "https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/$posh_binary" -O "$HOME/bin/$posh_binary" && chmod +x "$HOME/bin/$posh_binary" || exit 1
 
 echo "==> Verify oh-my-posh signature"
-if cosign verify-blob --key "$HOME/.shell/cosign/oh-my-posh.pub" --signature "$HOME/bin/$posh_binary.sig" "$HOME/bin/$posh_binary"; then
+if ~/bin/cosign verify-blob --key "$HOME/.shell/cosign/oh-my-posh.pub" --signature "$HOME/bin/$posh_binary.sig" "$HOME/bin/$posh_binary"; then
   mv "$HOME/bin/$posh_binary" "$HOME/bin/oh-my-posh"
   rm "$HOME/bin/$posh_binary.sig"
 else
