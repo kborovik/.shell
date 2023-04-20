7291b6ab8166b06e2b295bc3267d3a0f07343a5a
Konstantin Borovik <kb@lab5.ca>
2023-04-20 12:04:24 -0400

    add bash func

--- .bash-functions.sh
+++ .bash-functions.sh
@@ -10,13 +10,24 @@ jwt-decode() {
 }
 
 # List ServeAccount, Users, Groups for GCP project
-gcp-iam-project-member-roles() {
+gcp-list-project-member-roles() {
   if [ "${1}" ] && [ "${2}" ] && [ "$(command -v gcloud)" ]; then
-    gcloud projects get-iam-policy "${1}" \
+    gcloud projects get-iam-policy "${2}" \
       --flatten="bindings[].members" \
       --format="table(bindings.role)" \
-      --filter="bindings.members:${2}"
+      --filter="bindings.members:${1}"
   else
-    echo "Usage: ${FUNCNAME[0]} <project_id> <user|group|service_account>"
+    echo "Usage: ${FUNCNAME[0]} <user|group|service_account> <project_id>"
+  fi
+}
+
+# List GCP project members and filter out GCP service_accounts
+gcp-list-project-members() {
+  if [ "${1}" ] && [ "$(command -v gcloud)" ]; then
+    gcloud projects get-iam-policy "${1}" \
+      --flatten="bindings[].members" \
+      --format='value(bindings.members)' | sort -u | grep -vE 'developer.gserviceaccount.com|cloudservices.gserviceaccount.com|serviceAccount:service-'
+  else
+    echo "Usage: ${FUNCNAME[0]} <project_id>"
   fi
 }
