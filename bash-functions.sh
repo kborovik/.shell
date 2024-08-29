288dc69f21da2f488e25839220e93187bf42e2e3
Konstantin Borovik <kb@lab5.ca>
2024-08-29 14:47:39 -0400

    2024.08.29-1447

--- bash-functions.sh
+++ bash-functions.sh
@@ -1,62 +1,7 @@
 #!/usr/bin/env bash
 
-# Passwd generator
-gen-pass-16() {
-  gpg --gen-random --armor 1 32 | tr -d '/=+' | cut -c -16 | tr -d '\n'
-}
-
-gen-pass-32() {
+gen-pass() {
   gpg --gen-random --armor 1 64 | tr -d '/=+' | cut -c -32 | tr -d '\n'
-}
-
-# List ServeAccount, Users, Groups for GCP project
-gcp-list-project-member-roles() {
-  if [ "${1}" ] && [ "${2}" ] && [ "$(command -v gcloud)" ]; then
-    gcloud projects get-iam-policy "${2}" \
-      --flatten="bindings[].members" \
-      --format="table(bindings.role)" \
-      --filter="bindings.members:${1}"
-  else
-    echo "Usage: ${FUNCNAME[0]} <user|group|service_account> <project_id>"
-  fi
-}
-
-# List GCP project members and filter out GCP service_accounts
-gcp-list-project-members() {
-  if [ "${1}" ] && [ "$(command -v gcloud)" ]; then
-    gcloud projects get-iam-policy "${1}" \
-      --flatten="bindings[].members" \
-      --format='value(bindings.members)' |
-      sort -u |
-      grep -vE 'developer.gserviceaccount.com|cloudservices.gserviceaccount.com|serviceAccount:service-'
-  else
-    echo "Usage: ${FUNCNAME[0]} <project_id>"
-  fi
-}
-
-# List Organization ServeAccount, Users, Groups roles
-gcp-list-org-member-roles() {
-  if [ "${1}" ] && [ "${2}" ] && [ "$(command -v gcloud)" ]; then
-    gcloud organizations get-iam-policy "${2}" \
-      --flatten="bindings[].members" \
-      --format="table(bindings.role)" \
-      --filter="bindings.members:${1}"
-  else
-    echo "Usage: ${FUNCNAME[0]} <user|group|service_account> <organization_id>"
-  fi
-}
-
-# List GCP Organization members and filter out GCP service_accounts
-gcp-list-org-members() {
-  if [ "${1}" ] && [ "$(command -v gcloud)" ]; then
-    gcloud organizations get-iam-policy "${1}" \
-      --flatten="bindings[].members" \
-      --format='value(bindings.members)' |
-      sort -u |
-      grep -vE 'developer.gserviceaccount.com|cloudservices.gserviceaccount.com|serviceAccount:service-'
-  else
-    echo "Usage: ${FUNCNAME[0]} <organization_id>"
-  fi
 }
 
 # Get GCP Project ID
@@ -66,16 +11,93 @@ gcp-project-id() {
 
 # Get GCP Org ID
 gcp-org-id() {
-  project_id=$(gcloud config list --format='value(core.project)')
-  gcloud projects get-ancestors ${project_id} --format='value(id,type)' | grep organization | cut -f1
+  gcloud organizations list --format='value(name)'
 }
 
-# PDF Files
-pdf-optimize() {
-  if [ "${1}" ]; then
-    gs -sDEVICE=pdfwrite -dCompatibilityLevel=2.0 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile="${1%.pdf}-optimized.pdf" "${1}"
+# List GCP project members and filter out GCP service_accounts
+gcp-list-project-members() {
+  local google_project="$1"
+  
+  if [[ -z "$google_project" ]]; then 
+    google_project=$(gcloud config list --format='value(core.project)')
+  fi
+  
+  if [[ "$google_project" && "$(command -v gcloud)" ]]; then
+    echo "PROJECT: $google_project"
+    gcloud projects get-iam-policy "$google_project" \
+      --flatten="bindings[].members" \
+      --format='value(bindings.members)' | sort -u
   else
-    echo "Usage: ${FUNCNAME[0]} <input_file.pdf>"
+    echo "Usage: ${FUNCNAME[0]} <project_id>"
   fi
 }
 
+# List ServeAccount, Users, Groups for GCP project
+gcp-list-project-member-roles() {
+  local google_member="$1"
+  local google_project="$2"
+
+  if [[ -z "$google_member" ]]; then
+    echo echo "Usage: ${FUNCNAME[0]} <user|group|service_account> <project_id>"
+    return 1
+  fi
+  
+  if [[ -z "$google_project" ]]; then
+    google_project=$(gcloud config list --format='value(core.project)')
+  fi
+  
+  if [[ "$google_member" && "$google_project" && "$(command -v gcloud)" ]]; then
+    echo "PROJECT: $google_project"
+    echo "MEMBER: $google_member"
+    gcloud projects get-iam-policy "$google_project" \
+      --flatten="bindings[].members" \
+      --format="table(bindings.role)" \
+      --filter="bindings.members:$google_member"
+  else
+    echo "Usage: ${FUNCNAME[0]} <user|group|service_account> <project_id>"
+  fi
+}
+
+# List GCP Organization members and filter out GCP service_accounts
+gcp-list-org-members() {
+  local google_org="$1"
+
+  if [[ -z "$google_org" ]]; then 
+    google_org=$(gcloud organizations list --format='value(name)')
+  fi
+  
+  if [[ "$google_org" && "$(command -v gcloud)" ]]; then
+    echo "ORG: ${google_org}"
+    gcloud organizations get-iam-policy "$google_org" \
+      --flatten="bindings[].members" \
+      --format='value(bindings.members)' | sort -u
+  else
+    echo "Usage: ${FUNCNAME[0]} <organization_id>"
+  fi
+}
+
+# List Organization ServeAccount, Users, Groups roles
+gcp-list-org-member-roles() {
+  local google_member="${1}"
+  local google_org="${2}"
+
+  if [[ -z $google_member ]]; then 
+    echo "Usage: ${FUNCNAME[0]} <user|group|service_account> <organization_id>"
+    return 1
+  fi
+
+  if [[ -z ${google_org} ]]; then
+    google_org=$(gcloud organizations list --format='value(name)')
+  fi
+
+  if [[ "$google_member" && "${google_org}" && "$(command -v gcloud)" ]]; then
+    echo "ORG: ${google_org}"
+    echo "MEMBER: $google_member"
+    gcloud organizations get-iam-policy ${google_org} \
+      --flatten="bindings[].members" \
+      --format="table(bindings.role)" \
+      --filter="bindings.members:$google_member"
+  else
+    echo "Usage: ${FUNCNAME[0]} <user|group|service_account> <organization_id>"
+  fi
+}
