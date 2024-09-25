67bfac84dc4c9a440c2c8eed3c586786cdb9fd56
Konstantin Borovik <kb@lab5.ca>
2024-09-25 19:34:39 -0400

    2024.09.25-1934

--- Linux.mk
+++ Linux.mk
@@ -26,7 +26,7 @@ core-tools: apt-update tools posh bash git gpg vim
 
 $(local_bin):
 	$(call header,Local Bin directory)
-	mkdir -p $@
+	mkdir -p $(@)
 
 apt-update:
 	$(call header,APT - Update)
@@ -56,28 +56,34 @@ curl_bin := /usr/bin/curl
 pass_bin := /usr/bin/pass
 tree_bin := /usr/bin/tree
 unzip_bin := /usr/bin/unzip
+jq_bin := /usr/bin/jq
 
 $(curl_bin):
 	$(call header,curl - Install)
 	sudo apt-get --yes install curl
-	sudo touch $@
+	sudo touch $(@)
 
 $(pass_bin):
 	$(call header,pass - Install)
 	sudo apt-get --yes install pass
-	sudo touch $@
+	sudo touch $(@)
 
 $(tree_bin):
 	$(call header,tree - Install)
 	sudo apt-get --yes install tree
-	sudo touch $@
+	sudo touch $(@)
 
 $(unzip_bin):
 	$(call header,unzip - Install)
 	sudo apt-get --yes install unzip
-	sudo touch $@
+	sudo touch 
 
-tools: $(tree_bin) $(unzip_bin) $(pass_bin) $(curl_bin)
+$(jq_bin):
+	$(call header,jq - Install)
+	sudo apt-get --yes install jq
+	sudo touch 
+
+tools: $(tree_bin) $(unzip_bin) $(pass_bin) $(curl_bin) $(jq_bin)
 
 ###############################################################################
 # Git: Distributed version control system
@@ -88,7 +94,7 @@ git_bin := /usr/bin/git
 $(git_bin):
 	$(call header,Git - Install)
 	sudo apt-get --yes install git
-	sudo touch $@
+	sudo touch $(@)
 
 git: $(git_bin)
 	ln -rfs .gitconfig $(HOME)/.gitconfig
@@ -120,7 +126,7 @@ vim_bin := /usr/bin/vim
 $(vim_bin):
 	$(call header,Vim - Install)
 	sudo apt-get --yes install vim
-	sudo touch $@
+	sudo touch $(@)
 
 vim: $(vim_bin)
 	ln -rfs .vimrc $(HOME)/.vimrc
@@ -138,7 +144,7 @@ gpg_config := .gnupg/gpg.conf .gnupg/scdaemon.conf .gnupg/gpg-agent.conf
 $(scdaemon_bin):
 	$(call header,GPG - Install scdaemon)
 	sudo apt-get --yes install scdaemon
-	sudo touch $@
+	sudo touch $(@)
 
 $(gpg_dir):
 	$(call header,GPG - Directories)
@@ -148,7 +154,7 @@ $(gpg_dir):
 $(gpg_bin):
 	$(call header,GPG - Install)
 	sudo apt-get --yes install gnupg
-	sudo touch $@
+	sudo touch $(@)
 
 gpg: $(gpg_bin) $(scdaemon_bin) $(gpg_dir)
 	$(foreach file,$(gpg_config),ln -rfs $(file) $(HOME)/$(file);)
@@ -164,22 +170,22 @@ gcloud_apt_repo := /etc/apt/sources.list.d/google-packages.list
 
 $(gcloud_gpg_key):
 	$(call header,Terraform - GPG Public Key)
-	curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o $@
+	curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o $(@)
 
 $(gcloud_apt_repo):
 	$(call header,terraform - APT repository)
-	echo "deb [arch=amd64] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee $@
+	echo "deb [arch=amd64] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee $(@)
 	sudo apt update
 
 $(gcloud_bin): $(gcloud_gpg_key) $(gcloud_apt_repo)
 	$(call header,Google Cloud SDK - Install)
 	sudo apt-get --yes install google-cloud-sdk
-	sudo touch $@
+	sudo touch $(@)
 
 $(gke_auth_plugin): $(gcloud_bin)
 	$(call header,Google GKE Auth Plugin - Install)
 	sudo apt-get --yes install google-cloud-cli-gke-gcloud-auth-plugin
-	sudo touch $@
+	sudo touch $(@)
 
 gcloud: $(gcloud_bin) $(gke_auth_plugin)
 
@@ -192,7 +198,7 @@ kubectl_bin := /usr/bin/kubectl
 $(kubectl_bin):
 	$(call header,kubectl - Install)
 	sudo apt-get --yes install kubectl
-	sudo touch $@
+	sudo touch $(@)
 
 kubectl: $(kubectl_bin)
 
@@ -236,17 +242,17 @@ brave_bin := /usr/bin/brave-browser
 
 $(brave_gpg_key):
 	$(call header,Brave Browser - GPG Public Key)
-	curl -fsSL https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg | sudo gpg --dearmor -o $@
+	curl -fsSL https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg | sudo gpg --dearmor -o $(@)
 
 $(brave_apt_repo):
 	$(call header,Brave Browser - APT repository)
-	echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com stable main" | sudo tee $@
+	echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com stable main" | sudo tee $(@)
 	sudo apt update
 
 $(brave_bin): $(brave_gpg_key) $(brave_apt_repo)
 	$(call header,Brave Browser - Install)
 	sudo apt-get --yes install brave-browser
-	sudo touch $@
+	sudo touch $(@)
 
 brave: $(brave_bin)
 
@@ -259,11 +265,11 @@ hashicorp_apt_repo := /etc/apt/sources.list.d/hashicorp.list
 
 $(hashicorp_gpg_key):
 	$(call header,Hashicorp - GPG Public Key)
-	curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o $@
+	curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o $(@)
 
 $(hashicorp_apt_repo):
 	$(call header,Hashicorp - APT repository)
-	echo "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release) main" | sudo tee $@
+	echo "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release) main" | sudo tee $(@)
 	sudo apt update
 
 ###############################################################################
@@ -275,7 +281,7 @@ terraform_bin := /usr/bin/terraform
 $(terraform_bin): $(hashicorp_gpg_key) $(hashicorp_apt_repo)
 	$(call header,Terraform - Install)
 	sudo apt-get --yes install terraform
-	sudo touch $@
+	sudo touch $(@)
 
 terraform: $(terraform_bin)
 
@@ -289,17 +295,17 @@ docker_apt := /etc/apt/sources.list.d/docker.list
 
 $(docker_gpg):
 	$(call header,Docker - GPG Public Key)
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o $@
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o $(@)
 
 $(docker_apt):
 	$(call header,Docker - APT Repository)
-	echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release) stable" | sudo tee $@
+	echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release) stable" | sudo tee $(@)
 	sudo apt update
 
 $(docker_bin): $(docker_gpg) $(docker_apt)
 	$(call header,Docker - Install)
 	sudo apt-get --yes install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-	sudo touch $@
+	sudo touch $(@)
 
 docker: $(docker_bin)
 	sudo usermod -aG docker $(USER)
@@ -314,16 +320,16 @@ nvidia_hook := /usr/bin/nvidia-container-runtime-hook
 
 $(nvidia_gpg):
 	$(call header,NVIDIA - GPG Public Key)
-	curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o $@
+	curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o $(@)
 
 $(nvidia_apt):
 	$(call header,NVIDIA - APT repository)
-	echo "deb [signed-by=$(nvidia_gpg)] https://nvidia.github.io/libnvidia-container/stable/deb/$(ARCH) /" | sudo tee $@
+	echo "deb [signed-by=$(nvidia_gpg)] https://nvidia.github.io/libnvidia-container/stable/deb/$(ARCH) /" | sudo tee $(@)
 
 $(nvidia_hook): $(nvidia_gpg) $(nvidia_apt)
 	$(call header,NVIDIA - Install)
 	sudo apt-get --yes install nvidia-container-toolkit
-	sudo touch $@
+	sudo touch $(@)
 
 nvidia-config: $(docker)
 	if ! jq -e '.runtimes.nvidia.path' /etc/docker/daemon.json > /dev/null 2>&1; then
@@ -342,7 +348,7 @@ gh_bin := /usr/bin/gh
 $(gh_bin):
 	$(call header,gh - Install)
 	sudo apt-get --yes install gh
-	sudo touch $@
+	sudo touch $(@)
 
 gh: $(gh_bin)
 
@@ -355,7 +361,7 @@ pipx_bin := /usr/bin/pipx
 $(pipx_bin):
 	$(call header,pipx - Install)
 	sudo apt-get --yes install pipx
-	sudo touch $@
+	sudo touch $(@)
 
 pipx: $(pipx_bin)
 
@@ -369,12 +375,12 @@ ansible_lint := /home/kb/.local/bin/ansible-lint
 $(ansible_bin): $(pipx_bin)
 	$(call header,Ansible - Install)
 	pipx install ansible-core
-	touch $@
+	touch $(@)
 
 $(ansible_lint): $(pipx_bin)
 	$(call header,Ansible-Lint - Install)
 	pipx install ansible-lint
-	touch $@
+	touch $(@)
 
 ansible: $(ansible_bin) $(ansible_lint)
 
@@ -429,21 +435,21 @@ code_apt := /etc/apt/sources.list.d/vscode.list
 
 $(code_dir):
 	$(call header,Code - Directories)
-	mkdir -p $@
+	mkdir -p $(@)
 
 $(code_gpg):
 	$(call header,Code - Microsoft GPG Public Key)
-	curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o $@
+	curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o $(@)
 
 $(code_apt):
 	$(call header,Code - APT Repository)
-	echo "deb [arch=amd64] https://packages.microsoft.com/repos/code stable main" | sudo tee $@
+	echo "deb [arch=amd64] https://packages.microsoft.com/repos/code stable main" | sudo tee $(@)
 	sudo apt update
 
 $(code_bin): $(code_gpg) $(code_apt)
 	$(call header,Code - Install)
 	sudo apt-get --yes install code
-	sudo touch $@
+	sudo touch $(@)
 
 code: $(code_bin)
 	ln -rfs $(code_dir)/settings.json $(HOME)/$(code_dir)/settings.json
@@ -460,11 +466,11 @@ charm_apt_repo := /etc/apt/sources.list.d/charm.list
 
 $(charm_gpg_key):
 	$(call header,Charm - GPG Public Key)
-	curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o $@
+	curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o $(@)
 
 $(charm_apt_repo):
 	$(call header,Charm - APT repository)
-	echo "deb [signed-by=$(charm_gpg_key)] https://repo.charm.sh/apt/ * *" | sudo tee $@
+	echo "deb [signed-by=$(charm_gpg_key)] https://repo.charm.sh/apt/ * *" | sudo tee $(@)
 	sudo apt update
 
 ###############################################################################
@@ -479,7 +485,7 @@ $(mods_bin): $(charm_gpg_key) $(charm_apt_repo)
 	$(call header,Mods - Install)
 	sudo apt update
 	sudo apt-get --yes install mods
-	sudo touch $@
+	sudo touch $(@)
 
 mods: $(mods_bin) $(yq_bin)
 	$(call header,Mods - Configure)
@@ -505,7 +511,7 @@ ttyd_bin := $(HOME)/.local/bin/ttyd
 $(ffmpeg_bin):
 	$(call header,VHS - Install ffmpeg)
 	sudo apt-get --yes install ffmpeg
-	sudo touch $@
+	sudo touch $(@)
 
 $(ttyd_bin):
 	$(call header,VHS - Install ttyd)
@@ -519,7 +525,7 @@ $(ttyd_bin):
 $(vhs_bin): $(charm_gpg_key) $(charm_apt_repo)
 	$(call header,VHS - Install)
 	sudo apt-get --yes install vhs
-	sudo touch $@
+	sudo touch $(@)
 
 vhs: $(ffmpeg_bin) $(ttyd_bin) $(vhs_bin)
 
@@ -544,7 +550,7 @@ glow_bin := /usr/bin/glow
 $(glow_bin): $(charm_gpg_key) $(charm_apt_repo)
 	$(call header,Glow - Install)
 	sudo apt-get --yes install glow
-	sudo touch $@
+	sudo touch $(@)
 
 glow: $(glow_bin)
 
